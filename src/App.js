3ece5d99a1391849895a1694ef4719d835dc2c7c
rcruz60 <rcruz60@gmail.com>
2024-02-06 23:37:49 -0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 function App() {
   return (
     <div className="App">
       <>
-        <h1>Hello World!</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/offers" element={<Offers />} />            
+          </Routes>
+
+        </Router>
       </>
     </div>
   );
---
+++ src/pages/Offers.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Offers extends Component {
+  render() {
+    return (
+      <div>Offers</div>
+    )
+  }
+}
+
+export default Offers
