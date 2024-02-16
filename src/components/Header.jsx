947279fa471567e92e870feff84164d07c8de86c
rcruz60 <rcruz60@gmail.com>
2024-02-16 00:11:11 -0500

    create sign in, sign up and forgot password pages and oauth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
 
   function pathMathRoute(route) {
     if (route === location.pathname) {
+      console.log("true");
       return true;
     }
   }
@@ -25,25 +26,22 @@ export default function Header() {
         <div>
           <ul className="flex space-x-10">
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/") && "text-black border-b-red-500"
-              }`}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+              ${pathMathRoute("/") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/")}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/offers") && "text-black border-b-red-500"
-              }`}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+              ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/offers")}
             >
               Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/sign-in") && "text-black border-b-red-500"
-              }`}
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+              ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
               onClick={() => navigate("/sign-in")}
             >
               Sign In
